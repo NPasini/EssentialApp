c50ff23f82537b5a61b396517b5324539abf070a
Nicolò Pasini <eberron@hotmail.it>
2022-06-03 12:38:24 +0200

    Created FeedItem

---
+++ Packages/EssentialFeed/Sources/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  
+//
+//  Created by Nicolò Pasini on 03/06/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let imageURL: URL
+    let location: String?
+    let description: String?
+}
