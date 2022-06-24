f22a027566e3f02cb82b371794fdd111a3f28716
Nicolò Pasini <eberron@hotmail.it>
2022-06-24 16:38:02 +0200

    Separate Date extension methods into separate context

--- Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Cache/Helpers/SharedFeedCacheTestHelpers.swift
+++ Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Cache/Helpers/SharedFeedCacheTestHelpers.swift
@@ -23,17 +23,18 @@ extension Date {
 
     private var feedCacheMaxAgeInDays: Int { 7 }
 
-    func adding(days: Int) -> Date {
+    func minusFeedCacheMaxAge() -> Date {
+        self.adding(days: -feedCacheMaxAgeInDays)
+    }
+
+    private func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
+}
+
+extension Date {
 
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
     }
-
-    func minusFeedCacheMaxAge() -> Date {
-        self.adding(days: -feedCacheMaxAgeInDays)
-    }
 }
-
-
