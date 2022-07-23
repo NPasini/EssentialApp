430daa4abe925ed3a02848a574b76843b30624c2
Nicolò Pasini <eberron@hotmail.it>
2022-07-23 21:41:02 +0200

    Moved classes to their own files

--- Packages/EssentialFeed/Sources/EssentialFeediOSMVP/Feed UI/Composers/FeedMVPUIComposer.swift
+++ Packages/EssentialFeed/Sources/EssentialFeediOSMVP/Feed UI/Composers/FeedMVPUIComposer.swift
@@ -14,7 +14,7 @@ public enum FeedMVPUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         // In order to resolve circular dependencies we need to do property injection instead of constructor injection, the PresentationAdapter is a good candidate since is part of the composition
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
-        let feedController = FeedViewController.makeWith(
+        let feedController = makeWith(
             title: FeedPresenter.title,
             delegate: presentationAdapter
         )
@@ -29,95 +29,12 @@ public enum FeedMVPUIComposer {
 
         return feedController
     }
-}
 
-private extension FeedViewController {
     static func makeWith(title: String, delegate: FeedViewControllerDelegate) -> FeedViewController {
         let storyboard = UIStoryboard(name: "Feed", bundle: .module)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
         feedController.title = FeedPresenter.title
         feedController.delegate = delegate
         return feedController
-    }
-}
-
-private final class FeedViewAdapter: FeedView {
-
-    private let imageLoader: FeedImageDataLoader
-    private weak var controller: FeedViewController?
-
-    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
-        self.controller = controller
-        self.imageLoader = imageLoader
-    }
-
-    func display(_ viewModel: FeedViewModel) {
-        controller?.tableModel = viewModel.feed.map { model in
-            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
-            let view = FeedImageCellController(delegate: adapter)
-
-            adapter.presenter = FeedImagePresenter(
-                view: WeakRefVirtualProxy(view),
-                imageTransformer: UIImage.init
-            )
-            return view
-        }
-    }
-}
-
-private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-
-    var presenter: FeedPresenter?
-    private let feedLoader: FeedLoader
-
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-
-    func didRequestFeedRefresh() {
-        presenter?.didStartLoadingFeed()
-
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
-                self?.presenter?.didFinishLoadingFeed(with: feed)
-
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
-
-private final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
-
-    var presenter: FeedImagePresenter<View, Image>?
-
-    private let model: FeedImage
-    private var task: FeedImageDataLoaderTask?
-    private let imageLoader: FeedImageDataLoader
-
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-
-    func didRequestImage() {
-        presenter?.didStartLoadingImageData(for: model)
-
-        let model = self.model
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
-    }
-
-    func didCancelImageRequest() {
-        task?.cancel()
     }
 }
