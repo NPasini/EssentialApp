9091138a0ee549c2f9ef55ff8481e2c8eb7a363b
Nicolò Pasini <eberron@hotmail.it>
2022-07-24 18:41:10 +0200

    Update FeedImagePresenterTests.swift

--- Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -6,9 +6,35 @@
 //
 
 import XCTest
+import EssentialFeed
+import TestUtilities
+
+public protocol FeedImageView: AnyObject {
+    func display(_ viewModel: FeedImageViewModel)
+}
+
+public struct FeedImageViewModel {
+    let image: Any?
+    let isLoading: Bool
+    let location: String?
+    let shouldRetry: Bool
+    let description: String?
+
+    var hasLocation: Bool { location != nil }
+}
 
 public final class FeedImagePresenter {
 
+    private let view: FeedImageView
+
+    public init(view: FeedImageView) {
+        self.view = view
+    }
+
+    func didStartLoadingImageData(for model: FeedImage) {
+        let viewModel = FeedImageViewModel(image: nil, isLoading: true, location: model.location, shouldRetry: false, description: model.description)
+        view.display(viewModel)
+    }
 }
 
 final class FeedImagePresenterTests: XCTestCase {
@@ -19,23 +45,37 @@ final class FeedImagePresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expect no view messages")
     }
 
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+
+        sut.didStartLoadingImageData(for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertNil(message?.image)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedImagePresenter()
+        let sut = FeedImagePresenter(view: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
 
-    private class ViewSpy {
+    private class ViewSpy: FeedImageView {
 
-        enum Message: Hashable {
+        private(set) var messages = [FeedImageViewModel]()
+
+        func display(_ viewModel: FeedImageViewModel) {
+            messages.append(viewModel)
         }
-
-        private(set) var messages = Set<Message>()
-
-        
     }
 }
