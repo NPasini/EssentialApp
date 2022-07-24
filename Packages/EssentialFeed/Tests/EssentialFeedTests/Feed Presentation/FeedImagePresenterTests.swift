146172d8902e488bf948229ba01e5c6dd15fd44a
Nicolò Pasini <eberron@hotmail.it>
2022-07-24 18:58:26 +0200

    `FeedImagePresenter`displays loading image on `didStartLoadingImageData`

--- Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ Packages/EssentialFeed/Tests/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -10,11 +10,13 @@ import EssentialFeed
 import TestUtilities
 
 public protocol FeedImageView: AnyObject {
-    func display(_ viewModel: FeedImageViewModel)
+    associatedtype Image
+
+    func display(_ viewModel: FeedImageViewModel<Image>)
 }
 
-public struct FeedImageViewModel {
-    let image: Any?
+public struct FeedImageViewModel<Image> {
+    let image: Image?
     let isLoading: Bool
     let location: String?
     let shouldRetry: Bool
@@ -23,16 +25,24 @@ public struct FeedImageViewModel {
     var hasLocation: Bool { location != nil }
 }
 
-public final class FeedImagePresenter {
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
 
-    private let view: FeedImageView
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
 
-    public init(view: FeedImageView) {
+    public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
         self.view = view
+        self.imageTransformer = imageTransformer
     }
 
-    func didStartLoadingImageData(for model: FeedImage) {
-        let viewModel = FeedImageViewModel(image: nil, isLoading: true, location: model.location, shouldRetry: false, description: model.description)
+    public func didStartLoadingImageData(for model: FeedImage) {
+        let viewModel = FeedImageViewModel<Image>(image: nil, isLoading: true, location: model.location, shouldRetry: false, description: model.description)
+        view.display(viewModel)
+    }
+
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        let image = imageTransformer(data)!
+        let viewModel = FeedImageViewModel(image: image, isLoading: false, location: model.location, shouldRetry: false, description: model.description)
         view.display(viewModel)
     }
 }
@@ -60,21 +70,40 @@ final class FeedImagePresenterTests: XCTestCase {
         XCTAssertNil(message?.image)
     }
 
+    func test_didFinishLoadingImageData_displaysImage() {
+        let data = anyData()
+        let image = uniqueImage()
+        let transformedData = AnyImage()
+        let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
+
+        sut.didFinishLoadingImageData(with: data, for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertEqual(message?.image, transformedData)
+    }
+
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+    private func makeSUT(imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil }, file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, AnyImage>, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view)
+        let sut = FeedImagePresenter(view: view, imageTransformer: imageTransformer)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
 
+    private struct AnyImage: Equatable {}
+
     private class ViewSpy: FeedImageView {
 
-        private(set) var messages = [FeedImageViewModel]()
+        private(set) var messages = [FeedImageViewModel<AnyImage>]()
 
-        func display(_ viewModel: FeedImageViewModel) {
+        func display(_ viewModel: FeedImageViewModel<AnyImage>) {
             messages.append(viewModel)
         }
     }
