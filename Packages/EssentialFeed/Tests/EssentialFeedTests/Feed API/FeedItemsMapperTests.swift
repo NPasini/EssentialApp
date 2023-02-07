99e08fb60e9763fc26d03161ba5a001d2bf9047b
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-07 14:48:20 +0100

    Move test helpers to shared scope

--- Packages/EssentialFeed/Tests/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ Packages/EssentialFeed/Tests/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -86,15 +86,4 @@ class FeedItemsMapperTests: XCTestCase {
 
         return (model, json)
     }
-
-    private func makeItemsJson(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
 }
