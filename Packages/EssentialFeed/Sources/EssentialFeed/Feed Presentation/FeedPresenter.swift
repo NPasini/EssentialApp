2399fd47d0ccbf4ec50393d8b81ff5c4b14606f9
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-08 21:18:25 +0100

    Remove unused `FeedPresenter` logic

--- Packages/EssentialFeed/Sources/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ Packages/EssentialFeed/Sources/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -8,39 +8,10 @@
 import Foundation
 import iOSUtilities
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
 
     public static var title: String { Localized.Feed.title }
 
-    private let feedView: FeedView
-    private let errorView: ResourceErrorView
-    private let loadingView: ResourceLoadingView
-
-    public init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
-        self.feedView = feedView
-        self.errorView = errorView
-        self.loadingView = loadingView
-    }
-
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-
-    public func didFinishLoadingFeed(with error: Error) {
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-        errorView.display(ResourceErrorViewModel(errorMessage: Localized.Shared.loadError))
-    }
-    
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
         FeedViewModel(feed: feed)
     }
