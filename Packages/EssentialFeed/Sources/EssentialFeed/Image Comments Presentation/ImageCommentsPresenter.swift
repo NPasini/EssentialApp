f51c3329420d2f55e11da0ba7bc3b9a4440109b0
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-09 09:41:27 +0100

    Inject currentDate, locale, and calendar to make tests reliable in any locale

--- Packages/EssentialFeed/Sources/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
+++ Packages/EssentialFeed/Sources/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -13,12 +13,19 @@ public final class ImageCommentsPresenter {
 
     public static var title: String { Localized.ImageComments.title }
 
-    public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+    public static func map(
+        _ comments: [ImageComment],
+        currentDate: Date = Date(),
+        calendar: Calendar = .current,
+        locale: Locale = .current
+    ) -> ImageCommentsViewModel {
         let formatter = RelativeDateTimeFormatter()
+        formatter.calendar = calendar
+        formatter.locale = locale
         
         return ImageCommentsViewModel(comments: comments.map {
             ImageCommentViewModel(
-                date: formatter.localizedString(for: $0.createdAt, relativeTo: Date()),
+                date: formatter.localizedString(for: $0.createdAt, relativeTo: currentDate),
                 message: $0.message,
                 username: $0.username
             )
--- Packages/EssentialFeed/Tests/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ Packages/EssentialFeed/Tests/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -10,40 +10,47 @@ import EssentialFeed
 import TestUtilities
 
 class ImageCommentsPresenterTests: XCTestCase {
-
+    
     func test_title_isLocalized() {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE", table: "ImageComments"))
     }
     
     func test_map_createsViewModels() {
-            let now = Date()
-
-            let comments = [
-                ImageComment(
-                    id: UUID(),
-                    message: "a message",
-                    createdAt: now.adding(minutes: -5),
-                    username: "a username"),
-                ImageComment(
-                    id: UUID(),
-                    message: "another message",
-                    createdAt: now.adding(days: -1),
-                    username: "another username")
-            ]
-
-            let viewModel = ImageCommentsPresenter.map(comments)
-
-            XCTAssertEqual(viewModel.comments, [
-                ImageCommentViewModel(
-                    date: "5 minuti fa",
-                    message: "a message",
-                    username: "a username"
-                ),
-                ImageCommentViewModel(
-                    date: "1 giorno fa",
-                    message: "another message",
-                    username: "another username"
-                )
-            ])
-        }
+        let now = Date()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale = Locale(identifier: "en_US_POSIX")
+        
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "a message",
+                createdAt: now.adding(minutes: -5),
+                username: "a username"),
+            ImageComment(
+                id: UUID(),
+                message: "another message",
+                createdAt: now.adding(days: -1),
+                username: "another username")
+        ]
+        
+        let viewModel = ImageCommentsPresenter.map(
+            comments,
+            currentDate: now,
+            calendar: calendar,
+            locale: locale
+        )
+        
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(
+                date: "5 minutes ago",
+                message: "a message",
+                username: "a username"
+            ),
+            ImageCommentViewModel(
+                date: "1 day ago",
+                message: "another message",
+                username: "another username"
+            )
+        ])
+    }
 }
