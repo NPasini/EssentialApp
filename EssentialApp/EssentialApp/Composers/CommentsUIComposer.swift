1d955a9f1a9a0a75509a8d9f29e519ce3f9833c4
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-10 09:34:00 +0100

    Duplicate `FeedUIComposer` as `CommentsUIComposer`

---
+++ EssentialApp/EssentialApp/Composers/CommentsUIComposer.swift
@@ -0,0 +1,44 @@
+//
+//  CommentsUIComposer.swift
+//  EssentialApp
+//
+//  Created by nicolo.pasini on 10/02/23.
+//
+
+import UIKit
+import Combine
+import iOSUtilities
+import EssentialFeed
+import EssentialFeediOSMVP
+
+public enum CommentsUIComposer {
+
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    
+    public static func commentsComposedWith(commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) -> ListViewController {
+        // In order to resolve circular dependencies we need to do property injection instead of constructor injection, the PresentationAdapter is a good candidate since is part of the composition
+        let presentationAdapter = FeedPresentationAdapter(loader: commentsLoader)
+        
+        let feedController = makeWith(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+
+        presentationAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher() } 
+            ),
+            loadingView: WeakRefVirtualProxy(feedController),
+            errorView: WeakRefVirtualProxy(feedController),
+            mapper: FeedPresenter.map
+        )
+
+        return feedController
+    }
+
+    private static func makeWith(title: String) -> ListViewController {
+        let storyboard = UIStoryboard(name: "Feed", bundle: EssentialFeediOSMVPPackageBundle)
+        let feedController = storyboard.instantiateInitialViewController() as! ListViewController
+        feedController.title = title
+        return feedController
+    }
+}
