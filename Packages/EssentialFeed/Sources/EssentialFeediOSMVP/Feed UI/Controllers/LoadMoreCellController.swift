f396ca6aa1f79b2db0341fc502b3aaf32e099cdb
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-15 15:23:56 +0100

    Load more on error tap

--- Packages/EssentialFeed/Sources/EssentialFeediOSMVP/Feed UI/Controllers/LoadMoreCellController.swift
+++ Packages/EssentialFeed/Sources/EssentialFeediOSMVP/Feed UI/Controllers/LoadMoreCellController.swift
@@ -26,7 +26,16 @@ public final class LoadMoreCellController: NSObject, UITableViewDataSource, UITa
     }
     
     public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
+        reloadIfNeeded()
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        reloadIfNeeded()
+    }
+    
+    private func reloadIfNeeded() {
         guard !cell.isLoading else { return }
+        
         callback()
     }
 }
