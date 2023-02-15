fc56568dac9d83c3f12ffa09748391d95bb81507
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2023-02-15 10:24:36 +0100

    Does not load more after loading last page

--- EssentialApp/EssentialApp/Helpers/CombineHelpers.swift
+++ EssentialApp/EssentialApp/Helpers/CombineHelpers.swift
@@ -11,6 +11,21 @@ import EssentialFeed
 
 public extension Paginated {
     
+    init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
+        self.init(items: items, loadMore: loadMorePublisher.map { publisher in
+            return { completion in
+                // Usiamo Subscribers.Sink in modo tale che sia Combine a gestire il ciclo di vita della sottoscrizione e non dobbiamo gestire nessun Cancellable
+                publisher().subscribe(Subscribers.Sink(receiveCompletion: { result in
+                    if case let .failure(error) = result {
+                        completion(.failure(error))
+                    }
+                }, receiveValue: { result in
+                    completion(.success(result))
+                }))
+            }
+        })
+    }
+    
     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
         guard let loadMore = loadMore else { return nil }
         
